130a39031912c9fec3c3e9c8077bdd847b84da48
sanderengel <saet@itu.dk>
2026-05-02 18:27:04 +0200

    CHOR: Removed mistaken ML-like skeleton from compute_risk file

--- compute_risk.py
+++ compute_risk.py
@@ -8,7 +8,7 @@
 
 import numpy as np
 import pandas as pd
-from utils import load_cell_hours
+from utils import load_feature_grid
 
 
 
@@ -16,41 +16,4 @@ from utils import load_cell_hours
 ### LOAD DATA ###
 #################
 
-cell_hours = load_cell_hours()
-
-
-
-######################
-### TRANSFORM DATA ###
-######################
-
-# Log-transform lightning energy
-# TODO: MOVE INTO LOOP AFTER AGGREGATION
-cell_hours['energy_log'] = np.log1p(cell_hours['energy'])
-
-
-
-##########################
-### SPLIT INTO REGIONS ###
-##########################
-
-# Assign region 3 as the test set
-train = cell_hours[cell_hours['region'] != 3].copy()
-test = cell_hours[cell_hours['region'] == 3].copy()
-X_train, y_train = train[[col for col in train.columns if col != 'fire_onset']], train['fire_onset']
-X_test, y_test = test[[col for col in test.columns if col != 'fire_onset']], test['fire_onset']
-
-
-
-#################################
-### CROSS-VALIDATION TRAINING ###
-#################################
-
-# Define hyperparameter space
-CV_REGIONS = [1, 2, 4]  # Exclude region 3 for testing
-
-results = []
-
-for val_region in CV_REGIONS:
-    # TODO: Implement model
-    pass
+features = load_feature_grid()
